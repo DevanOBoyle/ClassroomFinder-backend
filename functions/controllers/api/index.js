4c359441892e4e6ca4058a7937d7c1715f48728a
Devan OBoyle <doboyle@ucsc.edu>
2023-02-01 00:36:56 -0800

    Added server functionality + basic get request

--- functions/controllers/api/index.js
+++ functions/controllers/api/index.js
@@ -1,11 +1,44 @@
+const client = require("../../utils/connection.js");
 const express = require("express");
+const cors = require("cors");
 
-const buildings = express();
-buildings.disable("x-powered-by");
-buildings.use(express.json());
+const app = express();
+app.disable("x-powered-by");
+app.use(express.json());
+
+const corsOptions = {
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 // API CALLS HERE
 
-const service = functions.https.onRequest(buildings);
+client.connect();
 
-module.exports = { buildings, service };
+app.get("/buildings", async (req, res) => {
+  try {
+    // TO DO: Get query headers from postgreSQL
+    /*
+    const buildings = await client.query("Get building data", (err, result));
+    const buildingDocs = [];
+    buildings.forEach((doc) => {
+      data = doc.data();
+      buildingDocs.push({
+        //TO DO
+        name: data.name,
+      });
+    });
+    
+    res.status(200).send({ status: 200, buildings: buildingDocs });
+    */
+    res.status(200).send({ status: 200, buildings: "Hello" });
+  } catch (err) {
+    res.status(500).send({ status: 500, error: "Unable to grab building data" });
+    functions.logger.log(`Unable to retrieve building data`);
+  }
+  //client.end;
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
